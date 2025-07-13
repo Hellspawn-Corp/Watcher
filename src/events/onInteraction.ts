bdda2f53d094444cb057349b75342a84899f0a78
effie <havealittlefaith2096@gmail.com>
2025-07-13 18:49:37 +0100

    forgot interaction

---
+++ src/events/onInteraction.ts
@@ -0,0 +1,15 @@
+import type { Interaction } from "discord.js";
+import { commandList } from "../commands/_CommandList";
+
+export const onInteraction = async (interaction: Interaction) => {
+	if (!interaction.isCommand()) {
+		return;
+	}
+
+	for await (const command of commandList) {
+		if (interaction.commandName === command.data.name) {
+			await command.run(interaction);
+			break;
+		}
+	}
+};
--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,18 @@ import { intentOptions } from "./config/IntentOptions";
 import { validateEnv } from "./utils/validateEnv";
 import { BotVars } from "./config/EnvironmentVars";
 import { onReady } from "./events/onReady";
+import { onInteraction } from "./events/onInteraction";
 
 (async () => {
 	validateEnv();
 	const bot = new Client({ intents: intentOptions });
+
 	bot.on("ready", async () => onReady(bot));
+
+	bot.on("interactionCreate", async (interaction) =>
+		onInteraction(interaction),
+	);
+
 	await bot.login(BotVars.BOT_TOKEN).then(() => {
 		bot.user?.setPresence({
 			activities: [{ name: "Skrrt Skrrrt", type: ActivityType.Playing }],
