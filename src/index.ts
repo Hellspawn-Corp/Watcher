4d2a3f66b12fa261fde83e92c49347b1c4830f50
Anna Thorne <annathorne@pm.me>
2025-07-18 20:37:07 +0200

    formatting

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import { ActivityType, Client } from "discord.js";
-import { BotVars } from "./config/EnvironmentVars";
 import { intentOptions } from "./config/IntentOptions";
-import { onInteraction } from "./events/onInteraction";
-import { onReady } from "./events/onReady";
 import { validateEnv } from "./utils/validateEnv";
+import { BotVars } from "./config/EnvironmentVars";
+import { onReady } from "./events/onReady";
+import { onInteraction } from "./events/onInteraction";
 
 (async () => {
 	validateEnv();
